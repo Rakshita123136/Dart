d8bf29c037f4c281f659e15ec5f62806fe6606b8
rakshita123136 <jayauppar2@gmail.com>
2025-01-18 22:45:52 +0530

    add files

--- classes/namedcons.dart
+++ classes/namedcons.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //named constructor can define addition construction with custom name
 class Point {
   late int x, y;
@@ -16,27 +16,3 @@ void main() {
   var point = Point.original();
   point.display();
 }
-/**The class name Point should start with an uppercase letter to follow Dart naming conventions.
-
-=======
-//named constructor can define addition construction with custom name
-class Point {
-  late int x, y;
-  //named constructor
-  Point.original() {
-    x = 0;
-    y = 0;
-  }
-  void display() {
-    print("point : ($x,$y)");
-  }
-}
-
-void main() {
-  var point = Point.original();
-  point.display();
-}
-/**The class name Point should start with an uppercase letter to follow Dart naming conventions.
-
->>>>>>> e890d62de975c10dae05323f002c18181a8466cf
- */
--- classes/static.dart
+++ classes/static.dart
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//static member belongs to the class not to individual objects
-
 class MathUtils {
   static const double pi = 3.14;
   static double CalculateCircleAreaa(double radius) {
@@ -11,19 +8,8 @@ class MathUtils {
 void main() {
   print("Pi : ${MathUtils.CalculateCircleAreaa}");
 }
-//Pi : Closure: (double) => double from Function 'CalculateCircleAreaa': static.gi
-=======
+//Pi : Closure: (double) => double from Function 'CalculateCircleAreaa': static.g
 //static member belongs to the class not to individual objects
 
-class MathUtils {
-  static const double pi = 3.14;
-  static double CalculateCircleAreaa(double radius) {
-    return pi * radius * radius;
-  }
-}
 
-void main() {
-  print("Pi : ${MathUtils.CalculateCircleAreaa}");
-}
-//Pi : Closure: (double) => double from Function 'CalculateCircleAreaa': static.
->>>>>>> e890d62de975c10dae05323f002c18181a8466cf
+
