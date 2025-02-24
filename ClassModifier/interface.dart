a7065c6445896942aa07385f4c4c417e7733230a
jyotika uppar <149363490+Rakshita123136@users.noreply.github.com>
2025-02-24 20:42:21 +0530

    Update interface.dart

--- ClassModifier/interface.dart
+++ ClassModifier/interface.dart
@@ -1,10 +1,19 @@
-//Interface  : Declare a class an interface which can only be implemened but had extended
-interface class Animal {}
+// **Interface Class**: Declares a class as an `interface`, meaning it **can only be implemented** but **not extended**.
+// This ensures that classes implementing the interface must provide their own implementation for all methods and properties.
 
+interface class Animal {
+  // An interface class typically contains method declarations (abstract methods), 
+  // but since no methods are defined here, it acts as a marker interface.
+}
+
+// The `Dog` class implements the `Animal` interface, meaning it must provide its own implementation.
+// Unlike `extends`, `implements` does **not** inherit default behavior from the interface.
 class Dog implements Animal {}
 
 void main() {
+  // Creating an instance of Dog
   var dog = Dog();
 
+  // Printing the Dog object (outputs the instance reference)
   print(dog);
 }
