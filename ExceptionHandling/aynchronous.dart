45c2c42f431f089bcb3659d51dda275bc42030ca
jyotika uppar <149363490+Rakshita123136@users.noreply.github.com>
2025-02-25 20:10:33 +0530

    Update aynchronous.dart

--- ExceptionHandling/aynchronous.dart
+++ ExceptionHandling/aynchronous.dart
@@ -1,13 +1,19 @@
-//handling error in Asynchronous cde dart uses futur and async / await for handling asynchronous code
-//you can handle exception in async function using try-catch block
+// **Handling Errors in Asynchronous Code**:  
+// Dart uses `Future` and `async/await` for handling asynchronous operations.  
+// When working with `async` functions, errors can be managed using a `try-catch` block to prevent crashes and handle failures gracefully.
+
 Future<void> fetchData() async {
   try {
+    // Simulating an asynchronous operation that fails
     throw Exception("Network error");
   } catch (e) {
-    print("Caught an error :$e");
+    // Catching and handling the exception
+    print("Caught an error: $e");
   }
 }
 
 void main() {
+  // Calling the asynchronous function
   fetchData();
 }
+
