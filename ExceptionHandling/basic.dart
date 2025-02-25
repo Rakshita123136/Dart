5fcb48397ec1d7cfd152d06899b07c04c8809ba2
jyotika uppar <149363490+Rakshita123136@users.noreply.github.com>
2025-02-25 20:11:34 +0530

    Update basic.dart

--- ExceptionHandling/basic.dart
+++ ExceptionHandling/basic.dart
@@ -1,8 +1,14 @@
 void main() {
   try {
-    var result = 10 ~/ 10;
+    // **Handling Division Operations Safely**:
+    // The `~/` operator performs integer division.
+    // If the denominator is zero, it throws an `IntegerDivisionByZeroException`.
+
+    var result = 10 ~/ 10; // No error occurs as 10 is divided by 10.
     print(result);
   } catch (e) {
-    print("error : $e");
+    // **Catching and Handling Exceptions**:
+    // If an error occurs (like division by zero), it will be caught here.
+    print("Error: $e");
   }
 }
